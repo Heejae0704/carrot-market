712638d27836b3433e9844f3f71d36045a44eab2
Heejae Chang <heejae0704@gmail.com>
2022-11-20 23:15:17 +0900

    authorization done

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,10 +1,62 @@
+import mail from '@sendgrid/mail';
+import twilio from 'twilio';
 import { NextApiRequest, NextApiResponse } from 'next';
-import withHandler from '@libs/server/withHandler';
+import withHandler, { ResponseType } from '@libs/server/withHandler';
 import client from '@libs/server/client';
 
-async function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log(req.body);
-  return res.status(200).end();
+mail.setApiKey(process.env.SENDGRID_API_KEY!);
+const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  const { phone, email } = req.body;
+  const user = phone ? { phone: phone } : email ? { email } : null;
+  if (!user) return res.status(400).json({ ok: false });
+  const payload = Math.floor(100000 + Math.random() * 900000) + '';
+  const token = await client.token.create({
+    data: {
+      payload,
+      user: {
+        connectOrCreate: {
+          where: {
+            ...user,
+          },
+          create: {
+            name: 'Anonymous',
+            ...user,
+          },
+        },
+      },
+    },
+  });
+
+  if (phone) {
+    // const message = await twilioClient.messages.create({
+    //   messagingServiceSid: process.env.TWILIO_SERVICE_SID,
+    //   to: process.env.TEST_PHONE!,
+    //   body: `Your login token is ${payload}.`,
+    // });
+    // console.log(message);
+  } else if (email) {
+    // const email = await mail.send({
+    //   from: 'heejae0704@gmail.com',
+    //   to: 'heejae0704@gmail.com',
+    //   subject: 'Your carrot market verification email',
+    //   text: `your token is ${payload}`,
+    //   html: `<strong>your token is ${payload}</strong>`,
+    // });
+    // console.log(email);
+  }
+
+  return res.json({
+    ok: true,
+  });
 }
 
-export default withHandler('POST', handler);
+export default withHandler({
+  method: 'POST',
+  handler,
+  isPrivate: false,
+});
